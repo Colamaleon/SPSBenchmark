6efdd7a152077ea2f716ba1f71635751735a26b4
Hanna <hsiek@mail.uni-paderborn.de>
2022-04-13 16:59:52 +0200

    disabled LaTeX export

--- src/main/java/spsbenchmark/SPSBenchmark.java
+++ src/main/java/spsbenchmark/SPSBenchmark.java
@@ -100,7 +100,7 @@ public class SPSBenchmark {
         // run the appropriate benchmark
         autoRunBenchmark();
 
-        BenchmarkExporter.exportBenchmarkTeX(this);
+        //BenchmarkExporter.exportBenchmarkTeX(this);
     }
 
 
